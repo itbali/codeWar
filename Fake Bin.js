edba238fccdb1b24a11d18c7f5f3c2d7c09c14dd
itbali <alex.itbali@gmail.com>
2022-01-13 00:13:04 +0300

    working only with text... need to fix

--- Fake Bin.js
+++ Fake Bin.js
@@ -1,3 +1,13 @@
-function FakeBin(str) {
-    console.log("start");
+let newStr = "";
+let stringOfNum = "252525252";
+function FakeBin(stringOfNumbers) {
+    let arrayOfNumbers = stringOfNumbers.split("");
+    arrayOfNumbers.forEach((element) => {
+        if (+element < 5) {
+            newStr += 0;
+        } else {
+            newStr += 1;
+        }
+    });
+    console.log(newStr);
 }
