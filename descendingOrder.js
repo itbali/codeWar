cfc479ccd551c31cc93761247acfebb44f5969bf
itbali <alex.itbali@gmail.com>
2022-01-14 23:58:20 +0300

    not log but return

--- descendingOrder.js
+++ descendingOrder.js
@@ -6,5 +6,5 @@ function descendingOrder(n) {
     sortedArray.forEach((element) => {
         sortedString = element + sortedString;
     });
-    console.log(sortedString);
+    return sortedString;
 }
