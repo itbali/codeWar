ceb1d7725c535c5a492bb91902dc90ce4ac5a5ab
itbali <alex.itbali@gmail.com>
2022-01-15 00:49:40 +0300

    random test fine

--- Pirates.js
+++ Pirates.js
@@ -1,15 +1,17 @@
-let a = { Mike: "aye", Joe: "nay", Johnson: "aye", Peter: "aye" };
-let numberOfPrirates = Object.keys(a).length;
-let i = 0;
-
-for (elem in a) {
-    console.log(a[elem]);
-    if (a[elem] == "aye") {
-        i = i + 1;
+const cannonsReady = (gunners) => {
+    let numberOfPrirates = Object.keys(gunners).length;
+    let i = 0;
+    let answer = "";
+    for (elem in gunners) {
+        if (gunners[elem] == "aye") {
+            i = i + 1;
+        }
     }
-}
-if (numberOfPrirates == i) {
-    console.log("Fire");
-} else {
-    console.log("Shiver me timbers!");
-}
+    if (numberOfPrirates == i) {
+        answer = "Fire!";
+    } else {
+        answer = "Shiver me timbers!";
+    }
+
+    return answer;
+};
