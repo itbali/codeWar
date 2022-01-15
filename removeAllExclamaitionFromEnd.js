eb50494fdbf7aa16f6e5f540c8b6d17958fa5eb7
itbali <alex.itbali@gmail.com>
2022-01-16 02:37:25 +0300

    looks like it works good

--- removeAllExclamaitionFromEnd.js
+++ removeAllExclamaitionFromEnd.js
@@ -1,13 +1,8 @@
 function remove(string) {
     let newString = "";
-    let n = 0;
     let arrayOfSymbols = string.split("");
-    while ((n = 1)) {
-        if (arrayOfSymbols.length - 1 == "!") {
-            arrayOfSymbols = arrayOfSymbols.slice(0, arrayOfSymbols.length - 1);
-        } else {
-            n = 1;
-        }
+    while (arrayOfSymbols[arrayOfSymbols.length - 1] == "!") {
+        arrayOfSymbols.splice(-1, 1);
     }
     arrayOfSymbols.forEach((element) => {
         newString = newString + element;
