d6aa96521457f77fd737a2e06bdea66da44ad915
itbali <alex.itbali@gmail.com>
2022-01-17 19:21:38 +0300

    created function that deletes expressions everywhere exept end of string

---
+++ Remove all exclamation marks from sentence except at the end.js
@@ -0,0 +1,16 @@
+let str = "Hi!! hi!!!";
+let n = str.length - 1;
+let newStr = "";
+
+while (str[n] == "!") {
+    newStr = str[n] + newStr;
+    n--;
+}
+while (n != 0) {
+    if (str[n] != "!") {
+        newStr = str[n] + newStr;
+        n--;
+    } else {
+        n--;
+    }
+}
