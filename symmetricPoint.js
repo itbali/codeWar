2891863615adeb9f33c1e4b045aa7c7b0311b711
itbali <alex.itbali@gmail.com>
2022-01-19 01:17:35 +0300

    firstCheck

--- symmetricPoint.js
+++ symmetricPoint.js
@@ -1,5 +1,16 @@
 function symmetricPoint(p, q) {
     let pd = [];
-    pd[0] = p[0] - q[0];
-    pd[1] = p[1] - q[1];
+
+    if (p[0] - q[0]) {
+        pd[0] = p[0] - q[0];
+    } else {
+        pd[0] = p[0] - q[0];
+    }
+
+    if (p[1] - q[1]) {
+        pd[1] = p[1] - q[1];
+    } else {
+        pd[1] = p[1] - q[1];
+    }
+    console.log(p);
 }
