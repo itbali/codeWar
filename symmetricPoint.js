41ad20f8a12db0a40298404145ad29433f0f13d0
itbali <alex.itbali@gmail.com>
2022-01-19 01:29:45 +0300

    works fine. needs test

--- symmetricPoint.js
+++ symmetricPoint.js
@@ -1,16 +1,21 @@
 function symmetricPoint(p, q) {
+    let p = [0, 0];
+    let q = [1, 1];
+
     let pd = [];
 
     if (p[0] - q[0]) {
-        pd[0] = p[0] - q[0];
+        pd[0] = q[0] - p[0];
     } else {
         pd[0] = p[0] - q[0];
     }
 
     if (p[1] - q[1]) {
-        pd[1] = p[1] - q[1];
+        pd[1] = q[1] - p[1];
     } else {
         pd[1] = p[1] - q[1];
     }
-    console.log(p);
+
+    let p1 = [q[0] + pd[0], q[1] + pd[1]];
+    console.log(p1);
 }
