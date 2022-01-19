fd6a3bfe38b5038996e4425b8af89ea5ba10fd69
itbali <alex.itbali@gmail.com>
2022-01-19 03:12:05 +0300

    1

---
+++ Largest Square Inside A Circle.js
@@ -0,0 +1,8 @@
+function areaLargestSquare(r) {
+    function areaLargestSquare(r) {
+        let gipotenusa = r * 2;
+        let singleSide = Math.sqrt((gipotenusa * gipotenusa) / 2);
+        let area = Math.floor(singleSide * singleSide);
+        return area;
+    }
+}
