49152881ad854f9d75a0fb3204a1ac4faebba4eb
itbali <alex.itbali@gmail.com>
2022-01-23 02:10:19 +0300

    check1

--- Keyboard Handler.js
+++ Keyboard Handler.js
@@ -1,45 +1,58 @@
-function handler(Key, isCaps = false, isShift = false) {
-    if (
-        Key.toString().length != 1 ||
-        typeof Key != "string" ||
-        (Key == Key.toUpperCase()) & isNaN(+Key)
-    ) {
-        return "KeyError";
-    } else {
-        if (isShift == true) {
-            if (isNaN(+Key)) {
-                return Key.toUpperCase();
-            } else {
-                switch (Key) {
-                    case "1":
-                        return "!";
-                    case "2":
-                        return "@";
-                    case "3":
-                        return "#";
-                    case "4":
-                        return "$";
-                    case "5":
-                        return "%";
-                    case "6":
-                        return "^";
-                    case "7":
-                        return "&";
-                    case "8":
-                        return "*";
-                    case "9":
-                        return "(";
-                    case "0":
-                        return ")";
+let Key = "a";
+let isCaps = true;
+let isShift = true;
 
-                        break;
+if (
+    Key.toString().length != 1 ||
+    typeof Key != "string" ||
+    (Key == Key.toUpperCase()) & isNaN(+Key)
+) {
+    return "KeyError";
+} else {
+    if (isShift == true) {
+        if (isNaN(+Key)) {
+            returnKey.toUpperCase();
+        } else {
+            switch (Key) {
+                case "1":
+                    return "!";
+                    break;
+                case "2":
+                    return "@";
+                    break;
+                case "3":
+                    return "#";
+                    break;
+                case "4":
+                    return "$";
+                    break;
+                case "5":
+                    return "%";
+                    break;
+                case "6":
+                    return "^";
+                    break;
+                case "7":
+                    return "&";
+                    break;
+                case "8":
+                    return "*";
+                    break;
+                case "9":
+                    return "(";
+                    break;
+                case "0":
+                    return ")";
+                    break;
 
-                    default:
-                        break;
-                }
+                default:
+                    return Key;
+                    break;
             }
-        } else if (isCaps == true) {
-            return Key.toString().toUpperCase();
         }
+    } else if (isCaps == true) {
+        return Key.toString().toUpperCase();
+    } else {
+        return Key;
     }
 }
