1ba558f72878db11ca74b019c7e83631331f17d5
itbali <alex.itbali@gmail.com>
2022-01-24 00:04:20 +0300

    read cearfully...

--- Student's Final Grade.js
+++ Student's Final Grade.js
@@ -1,9 +1,9 @@
 function finalGrade(exam, projects) {
     if (exam > 90 || projects > 10) {
         return 100;
-    } else if (exam > 75 || projects > 4) {
+    } else if ((exam > 75) & (projects > 4)) {
         return 90;
-    } else if (exam > 50 || projects > 1) {
+    } else if ((exam > 50) & (projects > 1)) {
         return 75;
     } else {
         return 0;
