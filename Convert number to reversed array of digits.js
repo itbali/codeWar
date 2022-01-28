55f11ca46c0d10cecdfc92a9f0270cd6307abdf2
itbali <alex.itbali@gmail.com>
2022-01-29 01:35:58 +0200

    works

---
+++ Convert number to reversed array of digits.js
@@ -0,0 +1,9 @@
+function digitize(n) {
+    //code here
+    let newar = [];
+    let ar = n.toString().split("").reverse();
+    ar.forEach((element) => {
+        newar.push(element);
+    });
+    return newar;
+}
