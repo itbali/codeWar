518bda944d65e7183adcf63b7c10b2dc634fd6fa
itbali <alex.itbali@gmail.com>
2022-01-30 00:00:58 +0200

    1

--- Simple Pig Latin.js
+++ Simple Pig Latin.js
@@ -1,8 +1,13 @@
 function pigIt(str) {
     let newar = str.split(" ");
     let collar = [];
+    let reg = /[a-zA-Z]/;
     newar.forEach((element) => {
-        collar.push((element + element[0] + "ay" + " ").substring(1));
+        if (reg.test(element)) {
+            collar.push((element + element[0] + "ay").substring(1));
+        } else {
+            collar.push(element);
+        }
     });
-    console.log(collar);
+    console.log(collar.join(" "));
 }
