9344d168cf5022f2461a6ed71a50afc5ffbc1746
itbali <alex.itbali@gmail.com>
2022-01-31 00:02:11 +0200

    fixed index

--- A Needle in the Haystack.js
+++ A Needle in the Haystack.js
@@ -1,3 +1,13 @@
 function findNeedle(haystack) {
     // your code here
+    haystack.forEach((element) => {
+        let i = 0;
+        console.log(element);
+        if (element == "needle") {
+            console.log("found the needle at position " + i);
+            i++;
+        } else {
+            i++;
+        }
+    });
 }
