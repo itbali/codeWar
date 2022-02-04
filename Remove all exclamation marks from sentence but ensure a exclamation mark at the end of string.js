7e0f991897ff1f4e4cdfb489ae8df72826df5f5f
itbali <alex.itbali@gmail.com>
2022-02-04 22:57:23 +0300

    2

--- Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
+++ Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.js
@@ -1,8 +1,3 @@
 function remove(string) {
-    if (string[string.length - 1] != "!") {
-        string = string.split("!").join("");
-    } else {
-        string = string.split("!").join("") + "!";
-    }
-    return string;
+    return string.split("!").join("") + "!";
 }
